ef7fd70588e6bc5f3034cad6ff061ee01bdbf9a4
Nicholas Babcock <Nicholas.Babcock@TLGcohort.com>
2021-06-23 15:48:54 -0700

    created questionLoader as a inner class

--- src/com/combatWombat/model/Question.java
+++ src/com/combatWombat/model/Question.java
@@ -1,5 +1,8 @@
 package com.combatWombat.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class Question {
@@ -60,5 +63,30 @@ public class Question {
         choiceMap.put("C", shuffledList.get(2));
         choiceMap.put("D", shuffledList.get(3));
         return shuffledList;
+    }
+}
+    class QuestionLoader {
+    private static final int MULTIPLE_CHOICE_AMOUNT = 4;
+    private Path dataFilePath;
+
+    public QuestionLoader(String dataFilePath) {
+        this.dataFilePath = Path.of(dataFilePath);
+    }
+    public List<Question> load() throws IOException {
+        List<Question> result = new ArrayList<>();
+        Files.lines(dataFilePath).forEach(line -> {
+            String[] tokens = line.split(",");
+            List<String> choices = new ArrayList<>();
+
+            String questionText = tokens[0];
+            Category category = Category.valueOf(tokens[1]);
+            String correctAnswer = tokens[2];
+
+            for(int i = 0; i < MULTIPLE_CHOICE_AMOUNT; i++){
+                choices.add(tokens[i + 2]);
+            }
+            result.add(new Question(questionText,correctAnswer,category,choices));
+        });
+        return result;
     }
 }
